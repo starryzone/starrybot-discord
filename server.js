0769eea4c1824ec17e0194d0829d72a33cc1ad05
Anselm Hook <anselm@gmail.com>
2021-11-21 20:16:10 -0800

    update names

--- server.js
+++ server.js
@@ -88,20 +88,20 @@ const getVoteCount = async (pool, candidate) => {
 // express glue
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const express = require('express');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
 // There's probably some settings we can customize for cors here
-app.use(cors());
+app.use(cors())
 
-app.enable('trust proxy');
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+app.enable('trust proxy')
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 app.use((req, res, next) => {
-	res.set('Content-Type', 'text/html');
-	next();
-});
+	res.set('Content-Type', 'text/html')
+	next()
+})
 
 let pool = 0;
 
@@ -110,13 +110,13 @@ app.use(async (req, res, next) => {
 		return next();
 	}
 	try {
-		pool = await createPoolAndEnsureSchema();
-		next();
+		pool = await createPoolAndEnsureSchema()
+		next()
 	} catch (err) {
-		logger.log('error',err);
-		return next(err);
+		logger.log('error',err)
+		return next(err)
 	}
-});
+})
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // a silly test
@@ -225,93 +225,39 @@ app.get('/tabs', async (req, res) => {
 	res.status(200).send(`Successfully voted for ${team} at ${timestamp}`).end();
 });
 
-app.get('/spaces', async (req, res) => {
-	pool = pool || (await createPoolAndEnsureSchema());
-
-	// Get the team from the request and record the time of the vote.
-	let team = "TABS"
-
-	const timestamp = new Date();
-
-	if (!team || (team !== 'TABS' && team !== 'SPACES')) {
-		res.status(400).send('Invalid team specified.').end();
-		return;
-	}
-
-	// Create a vote record to be stored in the database.
-	const vote = {
-		candidate: team,
-		time_cast: timestamp,
-	};
-
-	// Save the data to the database.
-	try {
-		await insertSomething(pool, vote);
-	} catch (err) {
-		logger.log('error',`Error while attempting to submit vote:${err}`);
-	res
-		.status(500)
-		.send('Unable to cast vote; see logs for more details.')
-		.end();
-		return;
-	}
-	res.status(200).send(`Successfully voted for ${team} at ${timestamp}`).end();
-});
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // the signing stuff
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const { serializeSignDoc } = require('@cosmjs/amino');
-const { Secp256k1, Secp256k1Signature, sha256 } = require('@cosmjs/crypto');
-const { fromBase64 } = require('@cosmjs/encoding');
+const { serializeSignDoc } = require('@cosmjs/amino')
+const { Secp256k1, Secp256k1Signature, sha256 } = require('@cosmjs/crypto')
+const { fromBase64 } = require('@cosmjs/encoding')
 
 app.get('/starry-backend', (req, res) => {
-  res.send({ express: 'I could have access to your database rows because I am a message from the backend. Sincerely, the backend' });
-});
+	res.send({ express: 'I could have access to your database rows because I am a message from the backend. Sincerely, the backend' });
+})
 
 app.post('/keplr-signed', (req, res) => {
-  console.log('req', req);
-  console.log('res', res);
-
-  let allIsGood = true;
-
-  if (allIsGood) {
-    res.sendStatus(200);
-  } else {
-    // Bad Request, you're grounded
-    res.sendStatus(400);
-  }
-});
+	let allIsGood = true
+	if (allIsGood) {
+		res.sendStatus(200)
+	} else {
+		// Bad Request, you're grounded
+		res.sendStatus(400)
+	}
+})
 
 const PORT = myconfig.PORT || 8080;
 const server = app.listen(PORT, () => {
-	logger.info(`App listening on port ${PORT}`);
-});
+	logger.info(`App listening on port ${PORT}`)
+})
 
-module.exports = server;
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Bare bones HTTP server - gcloud can mount this
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
-'use strict';
-const express = require('express')
-const app = express();
-app.use(express.static('public'))
-//app.get('/', (req, res) => { res.status(200).send('<h1>Hello, world!</h1>').end(); });
-const PORT = myconfig.PORT || 8080
-app.listen(PORT, () => {
-  logger.info(`App listening on port ${PORT}`)
-  logger.info('Press Ctrl+C to quit.')
-});
-module.exports = app
-*/
+module.exports = server
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 // The bot
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-/*
+
 const { Client, Intents } = require('discord.js')
 
 const intents = new Intents([ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ]);
@@ -322,16 +268,65 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
+
+	// ignore messages not from bots
 	if(message.author.bot) return
-	message.channel.send("saw message")
+
+	// ignore messages not with prefix
 	if(message.content.indexOf(myconfig.prefix) !== 0) return
+
+	// get message
 	const args = message.content.slice(myconfig.prefix.length).trim().split(/ +/g)
 	const command = args.shift().toLowerCase()
-	if(command === "ping") {
-	const m = await message.channel.send("Ping?")
+
+	// handle starme command
+	if(command === "starme") {
+
+		let uuid = message.author.uuid
+		
+
+		const m = await message.channel.send("Ping?")
 		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`)
+
 	}
+
+	if(command == "deleteme") {
+		// delete the user
+	}
+
+	// wait for !starme
+	//	uuid = get persons
+	// 	are they in the db?
+	//	if in the db
+	//		you are already in?
+	//	if not
+	// 		session = generate a session token
+	//		phrase = do a carl sagan ipsum; and make 3 sentences
+	//		timestamp = ?
+	//		write session and phrase to db
+	//		url = https://cosmos-webapp.pages.dev?session=session
+	//		print url to user as a dm -> send a dm
+	//
+
+
+	// https://cosmos-webapp.pages.dev/?session=session
+	//
+	// is this a valid uuid?
+	// ask db for carl sagan phrase
+	// check time stamp
+	// if error then say error
+	//
+	// if ok then
+	//		turn on sign button
+	//    signature = please sign this msg
+	//    blob = {
+	//		  session
+	//		  signed message
+	//	   	signature
+	//    }
+	//    send to https://queenbot.uc.r.appspot.com/starry-backend
+	//
+
 });
 
-client.login(config.token)
-*/
+client.login(myconfig.DISCORD_TOKEN)
