80a1f5d8e20f6c63166dcd855a75e8e5b2698580
Anselm Hook <anselm@gmail.com>
2021-11-21 21:12:04 -0800

    added starry command. added mock db. added sagan ipsum. added embed card

--- server.js
+++ server.js
@@ -225,8 +225,31 @@ app.get('/tabs', async (req, res) => {
 	res.status(200).send(`Successfully voted for ${team} at ${timestamp}`).end();
 });
 
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// the signing stuff
+// database wrapper - model
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let hack = {}
+
+let database = {
+	member_exists: (uuid) => {
+		return hack[uuid] ? true : false
+	},
+	member_add: (uuid,sessionid,quote,date) => {
+		hack[uuid] = {
+			sessionid: sessionid,
+			quote: quote,
+			date: date
+		}
+	},
+	member_delete: (uuid) => {
+		delete hack[uuid]
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// signing stuff
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const { serializeSignDoc } = require('@cosmjs/amino')
@@ -247,6 +270,10 @@ app.post('/keplr-signed', (req, res) => {
 	}
 })
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// actually start the express app
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 const PORT = myconfig.PORT || 8080;
 const server = app.listen(PORT, () => {
 	logger.info(`App listening on port ${PORT}`)
@@ -254,11 +281,215 @@ const server = app.listen(PORT, () => {
 
 module.exports = server
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// sagan ipsum
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let Ipsum = {
+  title: 'Sagan Ipsum - Now with Latin nebulas!',
+  head: 'Sagan Ipsum',
+  subhead: 'The Cosmos Awaits',
+  words: [
+  	'billions upon billions',
+  	'galaxies',
+  	'consciousness',
+  	'science',
+  	'light years',
+  	'cosmos',
+  	'rogue',
+  	'quasar',
+  	'culture',
+  	'explorations',
+  	'worldlets',
+  	'Vangelis',
+  	'a billion trillion',
+  	'Hypatia',
+  	'birth',
+  	'Flatland',
+  	'tesseract',
+  	'Rig Veda',
+  	'decipherment',
+  	'billions upon billions',
+  	'trillion',
+  	'radio telescope',
+  	'prime number',
+  	'colonies',
+  	'cosmic fugue',
+  	'extraplanetary',
+  	'corpus callosum',
+  	'Drake Equation',
+  	'Jean-François Champollion',
+  	'Cambrian explosion',
+  	'Tunguska event',
+  	'of brilliant syntheses',
+  	'circumnavigated',
+  	'Euclid',
+  	'Apollonius of Perga',
+  	'hydrogen atoms',
+  	'astonishment',
+  	'venture',
+  	'white dwarf'
+  ],
+
+  phrases : [
+  	'tingling of the spine',
+  	'tendrils of gossamer clouds',
+  	'ship of the imagination',
+  	'realm of the galaxies',
+  	'laws of physics',
+  	'rich in mystery',
+  	'cosmic ocean',
+  	'globular star cluster',
+  	'intelligent beings',
+  	'as a patch of light',
+  	'Orion\'s sword',
+  	'rings of Uranus',
+  	'preserve and cherish that pale blue dot',
+  	'brain is the seed of intelligence',
+  	'finite but unbounded',
+  	'at the edge of forever',
+  	'another world',
+  	'across the centuries',
+  	'how far away',
+  	'hundreds of thousands',
+  	'shores of the cosmic ocean',
+  	'encyclopaedia galactica',
+  	'muse about',
+  	'paroxysm of global death',
+  	'dispassionate extraterrestrial observer',
+  	'take root and flourish',
+  	'permanence of the stars',
+  	'kindling the energy hidden in matter',
+  	'hearts of the stars',
+  	'rich in heavy atoms',
+  	'great turbulent clouds',
+  	'gathered by gravity',
+  	'stirred by starlight',
+  	'inconspicuous motes of rock and gas',
+  	'the ash of stellar alchemy',
+  	'emerged into consciousness',
+  	'are creatures of the cosmos',
+  	'star stuff harvesting star light',
+  	'descended from astronomers',
+  	'dream of the mind\'s eye',
+  	'vanquish the impossible',
+  	'Sea of Tranquility',
+  	'from which we spring',
+  	'a still more glorious dawn awaits',
+  	'network of wormholes',
+  	'the sky calls to us',
+  	'concept of the number one',
+  	'not a sunrise but a galaxyrise',
+  	'bits of moving fluff',
+  	'something incredible is waiting to be known',
+  	'the carbon in our apple pies',
+  	'made in the interiors of collapsing stars',
+  	'extraordinary claims require extraordinary evidence',
+  	'vastness is bearable only through love',
+  	'a very small stage in a vast cosmic arena',
+    'invent the universe',
+  	'the only home we\'ve ever known',
+  	'a mote of dust suspended in a sunbeam',
+  	'courage of our questions',
+  	'citizens of distant epochs',
+  	'with pretty stories for which there\'s little good evidence',
+  	'two ghostly white figures in coveralls and helmets are soflty dancing'
+  ],
+  ending : ' and billions upon billions upon billions upon billions upon billions upon billions upon billions.',
+  more_lorem : ['Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium',
+                'totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo',
+                'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit',
+                'sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt',
+                'Neque porro quisquam est',
+                'qui dolorem ipsum quia dolor sit amet',
+                'consectetur',
+                'adipisci velit',
+                'sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem',
+                'Ut enim ad minima veniam',
+                'quis nostrum exercitationem ullam corporis suscipit laboriosam',
+                'nisi ut aliquid ex ea commodi consequatur',
+                'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur',
+                'vel illum qui dolorem eum fugiat quo voluptas nulla pariatur'],
+  puncuation: ['.','.','.','?','!']
+}
+
+function rando(limit) {
+  return Math.floor(Math.random()*limit);
+}
+
+function shuffle(words) {
+    var o, x;
+    var ix = words.length - 1;
+    var rtn = [];
+    while (ix) {
+        o = Math.floor(Math.random() * (ix + 1));
+        x = words[ix];
+        rtn.push(words[o]);
+        words[o] = x;
+        ix--;
+    }
+    rtn.push(words[0]);
+
+    return rtn;
+}
+
+function buildSentence(parts, puncuation) {
+  const shuffled = shuffle(parts).slice(0,6);
+  return shuffled.reduce( (accum, entry, ix) => {
+        let rtn = '';
+        if (ix === 0) {
+          rtn = accum + entry.charAt(0).toUpperCase() + entry.slice(1);
+        } else {
+          rtn = accum + ' ' + entry;
+        }
+
+        if (ix === shuffled.length - 1) {
+          rtn = rtn + puncuation[rando(puncuation.length - 1)];
+        }
+
+        return rtn;
+    }, '');
+}
+
+let buildSentence2 = function(isLastP=true, doLatin=false) {
+
+	const PSIZE = 32
+
+	let wordbase = Ipsum.words.concat(Ipsum.phrases);
+
+	if (doLatin) {
+		wordbase = wordbase.concat(Ipsum.more_lorem);
+	}
+
+	let text = '';
+	while ( (text.split(' ')).length < PSIZE) {
+		text += buildSentence(wordbase, Ipsum.puncuation) + ' ';
+	}
+
+	if (isLastP) {
+		text = text.substring(0, text.length - 2);
+		text += Ipsum.ending;
+	}
+
+	return text;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// SessionID
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var SessionID = function () {
+  // Math.random should be unique because of its seeding algorithm.
+  // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+  // after the decimal.
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 // The bot
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-const { Client, Intents } = require('discord.js')
+const { Client, Intents, MessageEmbed } = require('discord.js')
 
 const intents = new Intents([ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ]);
 const client = new Client({intents: intents }) 
@@ -268,30 +499,53 @@ client.on("ready", () => {
 });
 
 client.on("message", async message => {
-
-	// ignore messages not from bots
 	if(message.author.bot) return
+	if(message.content.indexOf(myconfig.PREFIX) !== 0) return
 
-	// ignore messages not with prefix
-	if(message.content.indexOf(myconfig.prefix) !== 0) return
-
-	// get message
-	const args = message.content.slice(myconfig.prefix.length).trim().split(/ +/g)
+	const args = message.content.slice(myconfig.PREFIX.length).trim().split(/ +/g)
 	const command = args.shift().toLowerCase()
 
-	// handle starme command
-	if(command === "starme") {
+	switch(command) {
+		case "starry":
 
-		let uuid = message.author.uuid
-		
+			let uuid = message.author.uuid
+			if(database.member_exists(uuid)) {
+				message.channel.send("You're already validated")
+				break
+			}
 
-		const m = await message.channel.send("Ping?")
-		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`)
+			let sessionid = SessionID()
+			let sagan = buildSentence2()
+			let url = `https://cosmos-webapp.pages.dev/?sessionid=${sessionid}`
 
-	}
+			database.member_add(uuid,sessionid,sagan,Date.now())
 
-	if(command == "deleteme") {
-		// delete the user
+			const exampleEmbed = new MessageEmbed()
+				.setColor('#0099ff')
+				.setTitle('Please visit: https://cosmos-webapp.pages.dev')
+				.setURL(url)
+				.setAuthor('Starrybot', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
+				.setDescription(sagan)
+				.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+				//.addFields(
+				//	{ name: 'Regular field title', value: 'Starry Invite' },
+				//	{ name: '\u200B', value: '\u200B' },
+				//	{ name: 'Inline field title', value: 'Acceleration on', inline: true },
+				//	{ name: 'Inline field title', value: 'Can you hear us Major Tom', inline: true },
+				//	{ name: 'Inline field title', value: sagan, inline: true },
+				//)
+				//.addField('Inline field title', 'Planet Earth is Blue', true)
+				//.setImage('https://i.imgur.com/AfFp7pu.png')
+				.setTimestamp()
+				.setFooter('Put your helmet on', 'https://i.imgur.com/AfFp7pu.png');
+
+			//message.channel.send({ embeds: [exampleEmbed] });
+			message.author.send({ embeds: [exampleEmbed] });
+			break
+		case "deleteme":
+			database.member_delete(message.author.uuid)
+			message.author.send("You've been brought back to earth")
+			break
 	}
 
 	// wait for !starme
