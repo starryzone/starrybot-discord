c581e86b08b3734fa0299d0ff8e89d68ccd84796
Anselm Hook <anselm@gmail.com>
2021-11-22 13:29:22 -0800

    added a concept of a discord room or server settings since the bot talks to many servers... also added hoisting of a user into a room and also notification with an invite

--- server.js
+++ server.js
@@ -142,13 +142,46 @@ const server = app.listen(PORT, () => {
 
 module.exports = server
 
+
+////////////////////////////////
+// Instance settings...
+//
+// TODO/TBD
+//
+// - the theory is that people will throw this bot in their server
+// - and they want to configure the bot for their server
+// - so i think this bot thing needs to deal with many servers and all kinds of data
+// - presumably there is some way to understand the current context
+// - and presumably we can store that in a db
+// - and allow config either by a website or maybe in the bot itself
+// - anyway, this code pretends to do that
+//
+////////////////////////////////
+
+let fakedb = {
+	default:{
+		validatorURL: "https://cosmos-webapp.pages.dev/?traveller=",
+		channelName:  "secretspecialchannel",
+		channelId: "846500999007043615",
+		//role:"futurian"
+	}
+}
+
+db.settingsAdd = (id,params) => {
+	fakedb[id]=params
+}
+
+db.settingsGet = (id="default") => {
+	return fakedb[id]
+}
+
 ////////////////////////////////
 // The Discord bot
 ////////////////////////////////
 
 const Sagan = require("./sagan.js")
 
-const { Client, Intents, MessageEmbed } = require('discord.js')
+const { Client, Intents, MessageEmbed, Permissions } = require('discord.js')
 const {myConfig} = require("./db");
 
 const intents = new Intents([ Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ]);
@@ -162,6 +195,10 @@ client.on("messageCreate", async message => {
 	if (message.author.bot) return
 	if (message.content.indexOf(db.myConfig.PREFIX) !== 0) return
 
+	// since this bot serves many masters, go ahead and find which server is involved here
+	let disco = db.settingsGet()
+
+	// peel a few details out of the message
 	const {guildId, author } = message;
 	const args = message.content.slice(db.myConfig.PREFIX.length).trim().split(/ +/g)
 	const command = args.shift().toLowerCase()
@@ -172,20 +209,25 @@ client.on("messageCreate", async message => {
 			const memberExists = await db.memberExists(uuid, guildId);
 
 			if (memberExists.length !== 0) {
+				// TODO it may be possible to ask discord rather than asking our own database - more stable
 				message.channel.send("You're already validated on this server :)")
 				break
 			}
+
+			// get a funny quote
 			let sagan = Sagan.sagan()
+
+			// create a session in db
 			let sessionId = await db.memberAdd({
 				discord_account_id: uuid,
 				discord_guild_id: guildId,
 				saganism: sagan
 			})
-			let url = `https://cosmos-webapp.pages.dev/?traveller=${sessionId}`
 
+			// tell user to go to validator site
 			const exampleEmbed = new MessageEmbed()
 				.setColor('#0099ff')
-				.setTitle(`Please visit: ${url}`)
+				.setTitle(`Please visit: ${disco.validatorURL+sessionId}`)
 				.setURL(url)
 				.setAuthor('Starrybot', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
 				.setDescription(sagan)
@@ -193,12 +235,51 @@ client.on("messageCreate", async message => {
 				.setTimestamp()
 				.setFooter('Put your helmet on', 'https://i.imgur.com/AfFp7pu.png');
 
-			await message.channel.send("Check your DM's");
+			// send it privately
 			await message.author.send({ embeds: [exampleEmbed] });
+
+			// but also tell them to check their dms publically
+			await message.channel.send("Check your DM's");
 			break
+
 		case "starry-delete":
-			await db.memberDelete(author.id, guildId)
+			// tell them first in case of crash
 			await message.channel.send("You've been brought back to earth. (And removed as requested.)")
+			// then attempt remove
+			await db.memberDelete(author.id, guildId)
+			break
+
+		case "starry-magic":
+
+			// As a test, immediately add the person who sent the message to the secret channel
+			const guild = await client.guilds.fetch(guildId)
+			const everyoneRole = guild.roles.everyone
+			const channel = await guild.channels.cache.get(disco.channelId)
+			await channel.permissionOverwrites.set([{ id: message.author.id , allow: ['VIEW_CHANNEL'] }])
+			logger.log("elevated perms")
+
+			// It makes sense to ALSO send them an invite code?
+			try {
+				const guild = await client.guilds.fetch(guildId);
+				const channel = await guild.channels.cache.get(disco.channelId);
+				const invite = await channel.createInvite({maxUses: 1 });
+				let url = `https://discord.gg/${invite.code}`
+				await message.author.send(url)
+			} catch(e) {
+				logger.error(e)
+			}
+			logger.log("invite sent")
+
+			// There is an idea of adding a "ROLE" to a user.
+			if(disco.role) {
+				let role = message.guild.roles.cache.find(r => r.name === disco.role)
+				if(!role) {
+					message.channel.send("Hmm, cannot find role " + disco.role)
+				} else {
+					let member = message.mentions.members.first();
+					message.member.roles.add(role).catch(console.error);
+				}
+			}
 			break
 	}
 
