082e7f6202f9fbd871813e481be550d741202bfb
Mike Purvis <mikedotexe@gmail.com>
2022-01-25 07:29:09 -0800

    add emoji reaction for user to enter DAODAO DAO URL

--- src/wizard/add-token-rule.js
+++ src/wizard/add-token-rule.js
@@ -33,7 +33,7 @@ function createStep1(userId, parentWizard) {
             new MessageEmbed()
               .setColor('#FDC2A0')
               .setTitle('Tell us about your token')
-              .setDescription('🌠 Choose a token\n✨ I need to make a token')
+              .setDescription('🌠 Choose a token\n✨ I need to make a token\n☯️ I want (or have) a DAO with a token')
           ],
           fetchReply: true
         }
@@ -41,13 +41,13 @@ function createStep1(userId, parentWizard) {
       try {
         await msg.react('🌠');
         await msg.react('✨');
+        await msg.react('☯️');
       } catch (error) {
         console.error('One of the emojis failed to react:', error);
       }
       return msg
     },
     async({ interaction }, ...extra) => {
-      console.log('inside step1 extra', extra)
       if (extra.length === 0) {
         console.error('Expected an emojiName field in extra args');
         return;
@@ -64,6 +64,10 @@ function createStep1(userId, parentWizard) {
           parentWizard.currentStep = parentWizard.steps[0]['optionSteps']['needsCW20']
           parentWizard.currentStep.beginFn({ interaction: null })
           break;
+        case '☯️':
+          parentWizard.currentStep = parentWizard.steps[0]['optionSteps']['daoDao']
+          parentWizard.currentStep.beginFn({ interaction: null })
+          break;
         default:
           console.warn('User did not pick an applicable emoji')
       }
@@ -71,52 +75,97 @@ function createStep1(userId, parentWizard) {
   )
 
   // This will check mainnet or testnet for the existence and balance of the cw20 contract
-  const checkForCW20 = async (cosmClient, cw20Input, isMainnet) => {
+  // gracefulExit is useful when we check if it's on mainnet first, then testnet.
+  //   if it's not on mainnet, we don't want it to fail, basically
+  const checkForCW20 = async (cosmClient, cw20Input, gracefulExit) => {
     let tokenInfo
     try {
       tokenInfo = await cosmClient.queryContractSmart(cw20Input, {
         token_info: { },
       })
     } catch (e) {
-      console.error('error message after trying to query cw20', e.message)
+      const chainId = await cosmClient.getChainId()
+      console.error(`rror message after trying to query cw20 on ${chainId}`, e.message)
       if (e.message.includes('decoding bech32 failed')) {
-        return await parentWizard.failure('Invalid address')
+        return await parentWizard.failure('Invalid address. Remember: first you copy, then you paste.')
       } else if (e.message.includes('contract: not found')) {
-        if (isMainnet) return false
-        return await parentWizard.failure('No contract at that address')
+        if (gracefulExit) return false
+        return await parentWizard.failure('No contract at that address. Potential black hole.')
       } else if (e.message.includes('Error parsing into type')) {
-        if (isMainnet) return false
-        return await parentWizard.failure('Entered valid contract that is not a cw20')
+        if (gracefulExit) return false
+        return await parentWizard.failure('That is a valid contract, but cosmic perturbations tell us it is not a cw20.')
       }
     }
     return tokenInfo
+  }
+
+  const checkForDAODAODAO = async (cosmClient, daoDAOUrl, gracefulExit) => {
+    let daoInfo
+    try {
+      const splitUrl = daoDAOUrl.split('/')
+      const daoAddress = splitUrl[splitUrl.length -1]
+      console.log('daoAddress', daoAddress)
+      daoInfo = await cosmClient.queryContractSmart(daoAddress, {
+        get_config: { },
+      })
+      console.log('daoInfo', daoInfo)
+    } catch (e) {
+      const chainId = await cosmClient.getChainId()
+      console.error(`Error message after trying to query daodao dao on ${chainId}`, e.message)
+      // TODO: reduce copy pasta
+      if (e.message.includes('decoding bech32 failed')) {
+        return await parentWizard.failure('Invalid address. Remember: first you copy, then you paste.')
+      } else if (e.message.includes('contract: not found')) {
+        if (gracefulExit) return false
+        return await parentWizard.failure('No contract at that address. Probable black hole.')
+      } else if (e.message.includes('Error parsing into type')) {
+        if (gracefulExit) return false
+        return await parentWizard.failure('That is a valid contract, but cosmic perturbations tell us it is not a cw20.')
+      }
+    }
+    return daoInfo
   }
 
   const handleCW20Entry = async ({interaction}, ...extra) => {
     // We may modify this, but for now we're just dealing with text inputs
     if (parentWizard.currentStep.interactionType !== 'text') return;
 
-    // Check user's cw20 token for existence on mainnet then testnet
-
-    const cw20Input = interaction.content;
+    // Check to see if they pasted a DAODAO URL like this:
+    // https://daodao.zone/dao/juno129spsp500mjpx7eut9p08s0jla9wmsen2g8nnjk3wmvwgc83srqq85awld
     let network = 'mainnet'
-    let cosmClient = await CosmWasmClient.connect(MAINNET_RPC_ENDPOINT)
-    let tokenInfo = await checkForCW20(cosmClient, cw20Input, true)
-    if (tokenInfo === false) {
-      // Nothing was found on mainnet, try testnet
-      network = 'testnet'
-      cosmClient = await CosmWasmClient.connect(TESTNET_RPC_ENDPOINT)
+    let cw20Input, tokenInfo, cosmClient, daoInfo
+    if (interaction.content.startsWith('https://daodao.zone')) {
+      cosmClient = await CosmWasmClient.connect(MAINNET_RPC_ENDPOINT)
+      daoInfo = await checkForDAODAODAO(cosmClient, interaction.content, true)
+      if (daoInfo === false) {
+        network = 'testnet'
+        cosmClient = await CosmWasmClient.connect(TESTNET_RPC_ENDPOINT)
+        daoInfo = await checkForDAODAODAO(cosmClient, interaction.content, false)
+      }
+
+      // If there isn't a governance token associated with this DAO, fail with message
+      if (!daoInfo || !daoInfo.hasOwnProperty('gov_token')) {
+        return await parentWizard.failure("We couldn't find any governance token associated with your DAO :/\nPerhaps destroyed in a supernova?")
+      }
+      cw20Input = daoInfo['gov_token']
+      // Now that we have the cw20 token address and network, get the info we want
       tokenInfo = await checkForCW20(cosmClient, cw20Input, false)
+    } else {
+      // Check user's cw20 token for existence on mainnet then testnet
+      cw20Input = interaction.content;
+      cosmClient = await CosmWasmClient.connect(MAINNET_RPC_ENDPOINT)
+      tokenInfo = await checkForCW20(cosmClient, cw20Input, true)
+      if (tokenInfo === false) {
+        // Nothing was found on mainnet, try testnet
+        network = 'testnet'
+        cosmClient = await CosmWasmClient.connect(TESTNET_RPC_ENDPOINT)
+        tokenInfo = await checkForCW20(cosmClient, cw20Input, false)
+      }
     }
 
     // If there were an error it would have returned a failure.
     // At this point we have the network and token info
     console.log('tokenInfo for user input', tokenInfo)
-
-    const daoInfo = await cosmClient.queryContractSmart('juno129spsp500mjpx7eut9p08s0jla9wmsen2g8nnjk3wmvwgc83srqq85awld', {
-      get_config: { },
-    })
-    console.log('daoInfo', daoInfo)
 
     parentWizard.state.cw20 = cw20Input
     parentWizard.state.network = network
@@ -183,6 +232,35 @@ function createStep1(userId, parentWizard) {
     },
     handleCW20Entry
   ))
+  step.addOptionStep('daoDao', new WizardStep(
+    parentWizard,
+    'text',
+    null,
+    async({interaction}, ...extra) => {
+      let guild = await step.parentWizard.client.guilds.fetch(parentWizard.guildId)
+      let channel = await guild.channels.fetch(parentWizard.channelId);
+      await channel.send({
+        embeds: [ new MessageEmbed()
+          .setColor('#FDC2A0')
+          .setTitle('Check out DAODAO')
+          .setURL('https://daodao.zone')
+          .setDescription("If you haven't set up a DAO, visit the link above to create a DAO with a governance token.")
+          .addFields([
+          {
+            name: '🧑‍🤝‍🧑', // a big space
+            value: '☯',
+            inline: false,
+          },
+          {
+            name: "Paste your DAODAO URL and we'll take care of the rest!",
+            value: "(For example, it'll look something like https://daodao.zone/dao/juno129spsp500mjpx7eut9p08s0jla9wmsen2g8nnjk3wmvwgc83srqq85awld)",
+          }
+        ])
+        ]
+      });
+    },
+    handleCW20Entry
+  ))
   return step
 }
 
@@ -203,7 +281,6 @@ function createStep2(userId, parentWizard) {
       });
     },
     async({ interaction }, ...extra) => {
-      console.log('inside step2 interaction', interaction)
       const amountOfTokensNeeded = interaction.content
       if (!Number.isInteger(parseInt(amountOfTokensNeeded)) || amountOfTokensNeeded <= 0 ) {
         // Invalid reply
