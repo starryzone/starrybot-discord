3a72408842932d388d81f6045ad8f48f1ba387e7
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-01-26 21:24:44 -0800

    Refactor embeds to use base createEmbeds helper

--- src/discord/utils.js
+++ src/discord/utils.js
@@ -4,28 +4,83 @@ const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js')
 let validatorURL = db.myConfig.VALIDATOR
 
 ///
-/// a helper to build display ux
+/// Helpers for consistent Discord UX in the bot
+///
+function createEmbed({
+    author,
+    color = '#0099ff',
+    description,
+    imageUrl,
+    footer,
+    setTimestamp,
+    title,
+    thumbnailUrl,
+    url,
+}) {
+    const embed = new MessageEmbed().setColor(color);
+    if (author) {
+        // Can be [author.name, author.thumbnailUrl, author.link]
+        if (Array.isArray(author)) {
+            embed.setAuthor(...author);
+        }
+        else {
+            embed.setAuthor(author);
+        }
+    }
+    if (description) {
+        embed.setDescription(description);
+    }
+    if (imageUrl) {
+        embed.setImage(imageUrl);
+    }
+    if (footer) {
+        // Can be [footer.text, footer.thumbnailUrl]
+        if (Array.isArray(footer)) {
+            embed.setFooter(...footer);
+        }
+        else {
+            embed.setFooter(footer);
+        }
+    }
+    if (setTimestamp) {
+        embed.setTimestamp();
+    }
+    if (title) {
+        embed.setTitle(title);
+    }
+    if (thumbnailUrl) {
+        embed.setThumbnail(thumbnailUrl);
+    }
+    if (url) {
+        embed.setURL(url);
+    }
+    return embed;
+}
+
+
+
+///
+/// Specific embeds used by the bot
 ///
 
 function createJoinEmbed(traveller, saganism) {
 	let url = `${validatorURL}?traveller=${traveller}`
-	return new MessageEmbed()
-		.setColor('#0099ff')
-		.setTitle(`Please visit ${url}`)
-		.setURL(url)
-		.setAuthor('StarryBot', 'https://i.imgur.com/AfFp7pu.png', 'https://discord.js.org')
-		.setDescription(saganism)
-		.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-		.setTimestamp()
-		.setFooter('Put your helmet on', 'https://i.imgur.com/AfFp7pu.png');
+    return createEmbed({
+        author: [`StarryBot`, `https://i.imgur.com/AfFp7pu.png`, `https://discord.js.org`],
+        description: saganism,
+        footer: [`Put your helmet on`, `https://i.imgur.com/AfFp7pu.png`],
+        title: `Please visit ${url}`,
+        thumbnailUrl: `https://i.imgur.com/AfFp7pu.png`,
+        url,
+    });
 }
 
 function createWelcomeEmbed(desiredRolesForMessage) {
-    return new MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(`Enable secure slash commands`)
-        .setDescription(`StarryBot just joined, and FYI there are some roles:\n- ${desiredRolesForMessage}`)
-        .setImage('https://starrybot.xyz/starrybot-slash-commands2.gif');
+    return createEmbed({
+        title: `Enable secure slash commands`,
+        description: `StarryBot just joined, and FYI there are some roles:\n- ${desiredRolesForMessage}`,
+        imageUrl: `https://starrybot.xyz/starrybot-slash-commands2.gif`,
+    });
 }
 
 function createWelcomeButton() {
