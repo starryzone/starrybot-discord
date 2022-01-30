ce963683856eba7699f5c43c99e7f8b38af5b877
Mike Purvis <mikedotexe@gmail.com>
2022-01-30 13:47:27 -0800

    token-add » token-rule, remove gives message, hide edit

--- src/commands/tokenRemove.js
+++ src/commands/tokenRemove.js
@@ -1,5 +1,17 @@
+const { createEmbed } = require("../utils/messages");
 
 async function starryCommandTokenRemove(interaction) {
+    if (interaction) {
+        return await interaction.reply({
+            embeds: [
+              createEmbed({
+                  title: 'This feature is a few light years away',
+                  description: 'Please come back later!'
+              })
+            ],
+            ephemeral: true
+        })
+    }
 }
 
 module.exports = {
