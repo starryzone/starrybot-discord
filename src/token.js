b18e185248a5d384e7669f373455541953be6353
Mike Purvis <mikedotexe@gmail.com>
2022-02-09 11:39:00 -0800

    return clever error messages to user for add token rule wizard

--- src/token.js
+++ src/token.js
@@ -1,7 +1,7 @@
 // This will check mainnet or testnet for the existence and balance of the cw20 contract
 // gracefulExit is useful when we check if it's on mainnet first, then testnet.
 //   if it's not on mainnet, we don't want it to fail, basically
-const checkForCW20 = async (cosmClient, cw20Input, gracefulExit) => {
+const checkForCW20 = async (parentWizard, cosmClient, cw20Input, gracefulExit) => {
   let tokenInfo
   try {
     tokenInfo = await cosmClient.queryContractSmart(cw20Input, {
@@ -12,10 +12,10 @@ const checkForCW20 = async (cosmClient, cw20Input, gracefulExit) => {
     tokenInfo = false
     console.error(`Error message after trying to query cw20 on ${chainId}`, e.message)
     if (e.message.includes('decoding bech32 failed')) {
-      throw 'Invalid address. Remember: first you copy, then you paste.';
+      return await parentWizard.failure('Invalid address. Remember: first you copy, then you paste.');
     } else if (e.message.includes('contract: not found')) {
       if (gracefulExit) return false
-      throw 'No contract at that address. Potential black hole.';
+      return await parentWizard.failure('No contract at that address. Potential black hole.');
     } else if (e.message.includes('Error parsing into type')) {
       if (gracefulExit) return false
       throw 'That is a valid contract, but cosmic perturbations tell us it is not a cw20.';
@@ -24,7 +24,7 @@ const checkForCW20 = async (cosmClient, cw20Input, gracefulExit) => {
   return tokenInfo
 }
 
-const checkForDAODAODAO = async (cosmClient, daoDAOUrl, gracefulExit) => {
+const checkForDAODAODAO = async (parentWizard, cosmClient, daoDAOUrl, gracefulExit) => {
   let daoInfo
   try {
     const splitUrl = daoDAOUrl.split('/')
@@ -39,13 +39,13 @@ const checkForDAODAODAO = async (cosmClient, daoDAOUrl, gracefulExit) => {
     console.error(`Error message after trying to query daodao dao on ${chainId}`, e.message)
     // TODO: reduce copy pasta
     if (e.message.includes('decoding bech32 failed')) {
-      throw 'Invalid address. Remember: first you copy, then you paste.';
+      return await parentWizard.failure('Invalid address. Remember: first you copy, then you paste.');
     } else if (e.message.includes('contract: not found')) {
       if (gracefulExit) return false
-      throw 'No contract at that address. Probable black hole.';
+      return await parentWizard.failure('No contract at that address. Probable black hole.');
     } else if (e.message.includes('Error parsing into type')) {
       if (gracefulExit) return false
-      throw 'That is a valid contract, but cosmic perturbations tell us it is not a cw20.';
+      return await parentWizard.failure('That is a valid contract, but cosmic perturbations tell us it is not a cw20.');
     }
   }
   return daoInfo
