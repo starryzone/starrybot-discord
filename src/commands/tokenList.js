ae6ac0407986fe925db355f6e4c85d388310c869
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-02-12 21:45:23 -0800

    ✨ Add new tokenList command to list the token rules per guild

---
+++ src/commands/tokenList.js
@@ -0,0 +1,30 @@
+const { rolesGet } = require("../db");
+const { createEmbed } = require("../utils/messages");
+
+async function starryCommandTokenList(req, res, ctx, next) {
+  const { interaction } = req;
+  let roles = await rolesGet(interaction.guildId);
+  const title = `${roles.length} roles found`;
+  const description = roles.length > 0 ?
+    `${roles.map(role => {
+        const roleName = role.give_role;
+        const roleAmt = role.has_minimum_of;
+        return `-${roleName} (min: ${roleAmt})\n`;
+      }).join('')}` :
+    `This will be way more exciting when roles are added :)`;
+
+  interaction.reply({
+    embeds: [
+      createEmbed({ title, description })
+    ]
+  })
+  res.done();
+}
+
+module.exports = {
+  starryCommandTokenList: {
+    name: 'list',
+    description: 'List all token rules for this guild',
+    execute: starryCommandTokenList,
+  }
+}
