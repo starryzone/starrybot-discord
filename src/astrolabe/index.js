6a30b167f70aaa8cdfed872b1991fd584f93f871
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-02-27 19:46:55 -0800

    🐛 Restore error messaging for invalid inputs

--- src/astrolabe/index.js
+++ src/astrolabe/index.js
@@ -20,16 +20,6 @@ const getTokenDetails = async (tokenInput) => {
   try {
     tokenDetails = await tokenType.getTokenDetails(tokenInput);
   } catch (e) {
-    // First try to log what chain failed
-    let chainId;
-    try {
-      // It's possible for this to also fail, but we still want to know what went wrong
-      chainId = await cosmClient.getChainId();
-    }  catch (e) {
-      chainId = '[chain ID not found]'
-    }
-    console.error(`Error message after trying to query ${currentTokenType.name} on ${chainId}`, e.message)
-
     // Throw a more specific error message if we can
     if (e.message.includes('decoding bech32 failed')) {
       throw 'Invalid address. Remember: first you copy, then you paste.';
@@ -37,6 +27,8 @@ const getTokenDetails = async (tokenInput) => {
       throw 'No contract at that address. Probable black hole.';
     } else if (e.message.includes('Error parsing into type')) {
       throw 'That is a valid contract, but cosmic perturbations tell us it is not a cw20.';
+    } else {
+      throw `Error message after trying to query ${tokenType.name}: ${e.message}`;
     }
   }
 
