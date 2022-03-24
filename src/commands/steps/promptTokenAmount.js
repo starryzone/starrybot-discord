5102ef26e20a9c416c4fd3dde6bb84f6cad5ddab
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-03-24 00:00:22 -0700

    Port promptToken steps

--- src/commands/steps/promptTokenAmount.js
+++ src/commands/steps/promptTokenAmount.js
@@ -1,51 +1,52 @@
-const { createEmbed } = require("../../utils/messages");
-
-async function promptTokenAmount(req, res, ctx, next) {
-	const { interaction } = req;
-  let amountOfTokensNeeded = interaction.content;
-
-  if (
-    !Number.isInteger(parseInt(amountOfTokensNeeded)) ||
-    amountOfTokensNeeded <= 0
-  ) {
-    // Invalid reply
-    return await res.error('Need a positive number of tokens.');
-  }
-
-  // Multiply by the decimals for native and fungible tokens
-  if (ctx.tokenType === 'native' || ctx.tokenType === 'cw20') {
-    console.log('Multiplying by the number of decimals', ctx.decimals)
-    ctx.minimumTokensNeeded = amountOfTokensNeeded * (10 ** ctx.decimals)
-    console.log('New amount needed', ctx.minimumTokensNeeded)
-  }
-
-  // Building the user friendly name for what they're making
-  let noun = `${amountOfTokensNeeded} `;
-  switch (ctx.tokenType) {
-    case('native'):
-      noun = `${noun} ${ctx.tokenSymbol}`;
-      break;
-    case('cw20'):
-      noun = `${noun} cw20 token(s)`;
-      break;
-    // cw721 doesn't go through this step
-    default:
-      noun = `${noun} token(s)`;
-      break;
-  }
-
-  await interaction.reply({
-    embeds: [
-      createEmbed({
-        title: 'What is the role name?',
-        description: `Please enter the name of the role that should be given to users with at least ${noun}.`,
-        footer: 'Note: this role will be created automatically',
-      }),
-    ]
-  });
-  next('promptTokenName')
-}
+const { buildBasicMessageCommand } = require('../../utils/commands');
 
 module.exports = {
-  promptTokenAmount,
+  promptTokenAmount: {
+    name: 'promptTokenAmount',
+    execute: buildBasicMessageCommand(async (req, res, ctx, next) => {
+      const { interaction } = req;
+      let amountOfTokensNeeded = interaction.content;
+
+      if (
+        !Number.isInteger(parseInt(amountOfTokensNeeded)) ||
+        amountOfTokensNeeded <= 0
+      ) {
+        // Invalid reply
+        return await res.error('Need a positive number of tokens.');
+      }
+
+      // Multiply by the decimals for native and fungible tokens
+      if (ctx.tokenType === 'native' || ctx.tokenType === 'cw20') {
+        console.log('Multiplying by the number of decimals', ctx.decimals)
+        ctx.minimumTokensNeeded = amountOfTokensNeeded * (10 ** ctx.decimals)
+        console.log('New amount needed', ctx.minimumTokensNeeded)
+      }
+
+      // Building the user friendly name for what they're making
+      let noun = `${amountOfTokensNeeded} `;
+      switch (ctx.tokenType) {
+        case('native'):
+          noun = `${noun} ${ctx.tokenSymbol}`;
+          break;
+        case('cw20'):
+          noun = `${noun} cw20 token(s)`;
+          break;
+        // cw721 doesn't go through this step
+        default:
+          noun = `${noun} token(s)`;
+          break;
+      }
+
+      return {
+        embeds: [
+          {
+            title: 'What is the role name?',
+            description: `Please enter the name of the role that should be given to users with at least ${noun}.`,
+            footer: 'Note: this role will be created automatically',
+          }
+        ],
+        next: 'promptTokenName',
+      }
+    })
+  }
 }
--- src/commands/steps/promptTokenName.js
+++ src/commands/steps/promptTokenName.js
@@ -1,41 +1,45 @@
+const { buildBasicMessageCommand } = require('../../utils/commands');
 const { rolesSet } = require("../../db");
 
-async function promptTokenName(req, res, ctx, next) {
-	const {
-    interaction: {
-      author,
-      content,
-      guild,
-      guildId,
-   }
-  } = req;
-  let roleToCreate = content;
-  const existingObjectRoles = await guild.roles.fetch();
-  let roleAlreadyExists = existingObjectRoles.some(role => role.name === roleToCreate);
-  if (roleAlreadyExists) {
-    // Invalid reply
-    return await res.error('A token role already exists with this name. Please pick a different name, or rename that one first.');
-  } else {
-    await guild.roles.create({name: roleToCreate, position: 0});
-  }
-
-  // Create database row
-  await rolesSet(
-    guildId,
-    roleToCreate,
-    ctx.tokenType,
-    ctx.tokenAddress,
-    ctx.network,
-    true,
-    author.id,
-    ctx.minimumTokensNeeded,
-    ctx.decimals,
-    ctx.stakingContract
-  );
-
-  res.done(`You may now use the role ${roleToCreate} for token-gated channels.\n\nEnjoy, traveller!`);
-}
-
 module.exports = {
-  promptTokenName,
+  promptTokenName: {
+    name: 'promptTokenName',
+    execute: buildBasicMessageCommand(async (req, res, ctx, next) => {
+      const {
+        interaction: {
+          author,
+          content,
+          guild,
+          guildId,
+       }
+      } = req;
+      let roleToCreate = content;
+      const existingObjectRoles = await guild.roles.fetch();
+      let roleAlreadyExists = existingObjectRoles.some(role => role.name === roleToCreate);
+      if (roleAlreadyExists) {
+        // Invalid reply
+        return await res.error('A token role already exists with this name. Please pick a different name, or rename that one first.');
+      } else {
+        await guild.roles.create({name: roleToCreate, position: 0});
+      }
+
+      // Create database row
+      await rolesSet(
+        guildId,
+        roleToCreate,
+        ctx.tokenType,
+        ctx.tokenAddress,
+        ctx.network,
+        true,
+        author.id,
+        ctx.minimumTokensNeeded,
+        ctx.decimals,
+        ctx.stakingContract
+      );
+
+      return {
+        done: `You may now use the role ${roleToCreate} for token-gated channels.\n\nEnjoy, traveller!`
+      }
+    }),
+  }
 }
