115847d1d1bd00745a0c72dac99f3f072f7a1f74
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-03-23 23:08:01 -0700

    Port editRoleAmount

--- src/commands/steps/editRoleAmount.js
+++ src/commands/steps/editRoleAmount.js
@@ -1,19 +1,19 @@
-const { createEmbed } = require("../../utils/messages");
-
-async function editRoleAmount(req, res, ctx, next) {
-  const { interaction } = req;
-  await interaction.reply({
-    embeds: [
-      createEmbed({
-        color: '#FDC2A0',
-        title: 'How many tokens?',
-        description: `Please enter the new amount of tokens a user needs to get the role named ${ctx.selectedRoleName} (current: ${ctx.selectedRole.has_minimum_of / (10 ** ctx.selectedRole.decimals)}).`,
-      })
-    ]
-  });
-  next('handleRoleAmountEdit')
-}
+const { buildBasicMessageCommand } = require('../../utils/commands');
 
 module.exports = {
-  editRoleAmount,
+  editRoleAmount: {
+    name: 'editRoleAmount',
+    execute: buildBasicMessageCommand(async (req, res, ctx, next) => {
+      return {
+        embeds: [
+          {
+            color: '#FDC2A0',
+            title: 'How many tokens?',
+            description: `Please enter the new amount of tokens a user needs to get the role named ${ctx.selectedRoleName} (current: ${ctx.selectedRole.has_minimum_of / (10 ** ctx.selectedRole.decimals)}).`,
+          }
+        ],
+        next: 'handleRoleAmountEdit',
+      }
+    })
+  }
 }
