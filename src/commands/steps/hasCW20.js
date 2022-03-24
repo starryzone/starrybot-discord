a4ffaf572914e88f9815a112d8c6431a4ae79e7a
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-03-23 23:48:24 -0700

    Port hasCW20

--- src/commands/steps/hasCW20.js
+++ src/commands/steps/hasCW20.js
@@ -1,19 +1,17 @@
-const { createEmbed } = require("../../utils/messages");
-
-async function hasCW20(req, res, ctx, next) {
-  const { interaction: { message: { channel } } } = req;
-  await channel.send({
-    embeds: [
-      createEmbed({
-        color: '#FDC2A0',
-        title: 'Enter your token address',
-        description: 'Please write your cw20 token address in Discord chat…',
-      })
-    ]
-  });
-  next('handleCW20Entry')
-}
+const { buildBasicMessageCommand } = require('../../utils/commands');
 
 module.exports = {
-  hasCW20,
+  hasCW20: {
+    name: 'hasCW20',
+    execute: buildBasicMessageCommand({
+      embeds: [
+        {
+          color: '#FDC2A0',
+          title: 'Enter your token address',
+          description: 'Please write your cw20 token address in Discord chat…',
+        }
+      ],
+      next: 'handleCW20Entry',
+    })
+  }
 }
