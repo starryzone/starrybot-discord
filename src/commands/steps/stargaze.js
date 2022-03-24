b38f326d4b79278739e0f5217fe3608aa0e32abe
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-03-24 00:08:14 -0700

    Port stargaze

--- src/commands/steps/stargaze.js
+++ src/commands/steps/stargaze.js
@@ -1,19 +1,17 @@
-const { createEmbed } = require("../../utils/messages");
-
-async function stargaze(req, res, ctx, next) {
-  const { interaction: { message: { channel } } } = req;
-  await channel.send({
-    embeds: [
-      createEmbed({
-        color: '#FDC2A0',
-        title: "Paste the Stargaze URL for the NFT collection and we'll take care of the rest!",
-        description: "(For example, it'll look something like https://app.stargaze.zone/launchpad/stars1lndsj2gufd292c35crv97ug2ncdcn9ys4s8e94wlxyeft6mt3k2svkwps9)",
-      })
-    ]
-  });
-  next('handleCW721Entry')
-}
+const { buildBasicMessageCommand } = require('../../utils/commands');
 
 module.exports = {
-  stargaze,
+  stargaze: {
+    name: 'stargaze',
+    execute: buildBasicMessageCommand({
+      embeds: [
+        {
+          color: '#FDC2A0',
+          title: "Paste the Stargaze URL for the NFT collection and we'll take care of the rest!",
+          description: "(For example, it'll look something like https://app.stargaze.zone/launchpad/stars1lndsj2gufd292c35crv97ug2ncdcn9ys4s8e94wlxyeft6mt3k2svkwps9)",
+        }
+      ],
+      next: 'handleCW721Entry'
+    })
+  },
 }
