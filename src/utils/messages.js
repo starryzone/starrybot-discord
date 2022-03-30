bf54e90ed8cb75b256ef8dd3139c69dabe55c657
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-03-29 21:36:01 -0700

    Initialize Wizardware Framework
    - New Wizardware and Wizard classes similar to the previous
      Wizard (and omitted WizardStep) classes from before.
    - New commands utility function that holds logic used to
      power the wizardware - in the future, this may be refactored
      to be half built into the wizard, half configured for the
      discord version of the bot.
    - Updates to the `createMessages` helper to allow it to be the
      main entry point for all other discord messages; this allows us
      to refactor the rest of the code to not consider messages in
      terms of "embeds" or "buttons" or other discord-specific
      concepts.

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,4 +1,10 @@
-const { MessageActionRow, MessageButton, MessageEmbed, MessagePayload } = require('discord.js')
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js')
+
+const COLORS_BY_MESSAGE_TYPE = {
+  error: '#BE75A4',
+  success: '#7485FF',
+  prompt: '#FDC2A0',
+}
 
 ///
 /// Helpers for consistent Discord UX in the bot
@@ -82,17 +88,64 @@ function createMessageActionRow({
 }
 
 function createMessage({
+  buttons, // TO-DO: Not a discord concept
   content,
-  components,
   embeds,
-  user,
+  ephemeral,
+  fetchReply, // Set to true if you need to react
 }) {
-  return MessagePayload.create(user, { content, components, embeds });
+
+  let componentPayload;
+  if (buttons?.length > 0) {
+    const row = createMessageActionRow({
+      components: buttons.map(buttonConfig => createButton(buttonConfig)),
+    });
+    componentPayload = [row];
+  }
+
+  let embedPayload;
+  if (embeds?.length > 0) {
+    embedPayload = embeds.map(embed => createEmbed(embed));
+  }
+
+  // We don't use MessagePayload here because it requires passing in
+  // a user first, and right now we're conveniently only ever using
+  // this object in an interaction.reply
+  return {
+    content,
+    components: componentPayload,
+    embeds: embedPayload,
+    ephemeral,
+    fetchReply,
+  };
+}
+
+function createError(errorMessage, ephemeral) {
+  return createMessage({
+    embeds: [
+      {
+        color: COLORS_BY_MESSAGE_TYPE.error,
+        title: 'Error (star might be in retrograde)',
+        description: errorMessage,
+      }
+    ],
+    ephemeral,
+  })
+}
+
+function createPrivateError(errorMessage) {
+  return createError(errorMessage, true);
 }
 
 module.exports = {
+  COLORS_BY_MESSAGE_TYPE,
+
+  // Naive discord wrappers
   createButton,
   createEmbed,
   createMessage,
   createMessageActionRow,
+
+  // Meaningful/reusable components
+  createPrivateError,
 }
---
+++ src/wizardware/index.js
@@ -0,0 +1,61 @@
+const { Wizard } = require("./wizard");
+
+class Wizardware {
+  activeWizards = new Map();
+
+  dependencies = {};
+
+  registeredSteps = new Map();
+
+  handleError;
+
+  timeoutDuration = 360000;
+
+  constructor({ dependencies, handleError, timeoutDuration }) {
+    if (handleError) {
+      this.handleError = handleError;
+    }
+
+    if (dependencies) {
+      this.dependencies = Object.assign(this.dependencies, dependencies);
+    }
+
+    if (timeoutDuration) {
+      this.timeoutDuration = timeoutDuration;
+    }
+  }
+
+  registerStep(name, definition) {
+    this.registeredSteps.set(name, definition);
+  }
+
+  async initiate(uniqueKey, commandName, initialState) {
+    const newWizard = new Wizard({
+      uniqueKey,
+      wizardware: this,
+    });
+    newWizard.execute(commandName, initialState);
+  }
+
+  async continue(uniqueKey, state) {
+    if (!this.activeWizards.has(uniqueKey)) return;
+    const wizard = this.activeWizards.get(uniqueKey);
+    const nextStep = typeof wizard.getNextStep === 'string' ?
+      wizard.getNextStep :
+      await wizard.getNextStep(state);
+    await wizard.execute(nextStep, state);
+  }
+
+  async end (uniqueKey) {
+    this.activeWizards.delete(uniqueKey);
+  }
+
+  async error (uniqueKey, errorMessage) {
+    await this.handleError(errorMessage);
+    this.end(uniqueKey);
+  }
+}
+
+module.exports = {
+  Wizardware,
+}
