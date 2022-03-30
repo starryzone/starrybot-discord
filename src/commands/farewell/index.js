5e5f21652e981c1f967756b911ca48d240e43f89
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-03-29 21:53:51 -0700

    Refactor all steps with button prompts

--- src/commands/farewell/index.js
+++ src/commands/farewell/index.js
@@ -1,43 +1,29 @@
 const { farewellConfirmation } = require('./farewellConfirmation');
 const { farewellRejection } = require('./farewellRejection');
-const { createButton, createMessageActionRow } = require("../../utils/messages");
-
-// Farewell
-async function starryCommandFarewell(req, res, ctx, next) {
-  const { interaction } = req;
-
-  const row = createMessageActionRow({
-    components: [
-      createButton({
-        customId: 'farewellConfirmation',
-        label: 'I understand',
-        style: 'PRIMARY',
-      }),
-      createButton({
-        customId: 'farewellRejection',
-        label: 'Cancel',
-        style: 'SECONDARY',
-      }),
-    ]
-  });
-
-  await interaction.reply({
-    content: 'This will delete roles created by starrybot.',
-    components: [row]
-  });
-
-  next(interaction => interaction.customId);
-}
 
 module.exports = {
   starryCommandFarewell: {
     adminOnly: true,
     name: 'farewell',
-    description: '(Admin only) Kick starrybot itself from your guild',
-    execute: starryCommandFarewell,
+    description: 'Kick starrybot itself from your guild',
+    prompt: {
+      type: 'button',
+      title: 'This will delete roles created by starrybot.',
+      options: [
+        {
+          next: 'farewellConfirmation',
+          label: 'I understand',
+        },
+        {
+          next: 'farewellRejection',
+          label: 'Cancel',
+          style: 'SECONDARY',
+        }
+      ]
+    },
     steps: {
       farewellConfirmation,
       farewellRejection,
-    },
+    }
   }
 }
