41eaec255fe0d2d2bd71cc6f2dff1441794cc752
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-03-29 21:44:46 -0700

    Prop up Wizardware installation
    - The src/commands/index file now creates new wizardware and passes
      it into buildCommandData for the steps to be registered by
      utils/messages. This replaces the flattenedCommandMap and
      globalCommandChains patterns with the wizardware's own
      registeredSteps and activeWizards map.
    - The 3 event handlers that were responsible for interacting with
      the command chain previously (on slash command, message send, and
      emoji reaction) now continue the wizard instead.

--- src/handlers/messageCreate.js
+++ src/handlers/messageCreate.js
@@ -1,10 +1,14 @@
-const { continueCommandChain } = require("../commands/index.js");
+const { wizardware } = require("../commands");
 
 async function messageCreate(interaction) {
-  if (interaction.author.bot) return;
-  await continueCommandChain({sourceAction: interaction});
+	if (interaction.author.bot) return;
+
+	await wizardware.continue(
+		`${interaction.guildId}-${interaction.author.id}`,
+		{ interaction, userInput: interaction.content }
+	);
 }
 
 module.exports = {
-  messageCreate,
+    messageCreate,
 }
--- src/handlers/messageReactionAdd.js
+++ src/handlers/messageReactionAdd.js
@@ -1,22 +1,26 @@
-const { continueCommandChain } = require("../commands");
+const { wizardware } = require("../commands");
 
 // A user may have sent an emoji - we are very interested in these
 async function messageReactionAdd(reaction, user) {
-  if (user.bot) return; // don't care about bot's emoji reactions
-  await continueCommandChain({sourceAction: reaction, user});
+	if (user.bot) return; // don't care about bot's emoji reactions
 
-  // When a reaction is received, check if the structure is partial
-  if (reaction.partial) {
-    // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
-    try {
-      const fetched = await reaction.fetch();
-      console.log(fetched);
-    } catch (error) {
-      console.error('Something went wrong when fetching the message:', error);
-    }
-  }
+	await wizardware.continue(
+		`${reaction.message.guildId}-${user.id}`,
+		{ reaction, user }
+	);
+
+	// When a reaction is received, check if the structure is partial
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+		try {
+			const fetched = await reaction.fetch();
+			console.log(fetched);
+		} catch (error) {
+			console.error('Something went wrong when fetching the message:', error);
+		}
+	}
 }
 
 module.exports = {
-  messageReactionAdd,
+    messageReactionAdd,
 }
