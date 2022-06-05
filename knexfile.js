d18809056e63074ce8e699c42b9ac661770cb6bf
juice me <shawn@hatswapcity.com>
2022-06-05 19:08:15 -0400

    fixing knex file

--- knexfile.js
+++ knexfile.js
@@ -7,43 +7,6 @@ const enableSSL = !['localhost', '127.0.0.1'].includes(myConfig.DB_HOSTIP);
 
 // TODO: we can review the various environments below
 module.exports = {
-  development: {
-    client: 'postgresql',
-    connection: {
-      user: myConfig.DB_USER,
-      password: myConfig.DB_PASS,
-      database: myConfig.DB_NAME,
-      host: myConfig.DB_HOSTIP,
-      port: myConfig.DB_HOSTPORT,
-      ssl: { rejectUnauthorized: false }
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      user: myConfig.DB_USER,
-      password: myConfig.DB_PASS,
-      database: myConfig.DB_NAME,
-      host: myConfig.DB_HOSTIP,
-      port: myConfig.DB_HOSTPORT,
-      ssl: enableSSL
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
   production: {
     client: 'postgresql',
@@ -53,7 +16,7 @@ module.exports = {
       database: myConfig.DB_NAME,
       host: myConfig.DB_HOSTIP,
       port: myConfig.DB_HOSTPORT,
-      ssl: enableSSL
+      ssl: { rejectUnauthorized: false }
     },
     pool: {
       min: 2,
