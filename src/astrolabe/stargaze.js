7c064c913b0df9530162cc71e441e587b3ef6ccc
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-09-29 18:42:08 -0500

    Update stargaze CW721 handling (#110)
    The stargaze launchpad URLs are slightly different if you navigate
    to them from the homepage now, causing our (strict) regular
    expressions to no longer recognize them. This commit updates the
    regex to expect either of the patterns we now know of.

--- src/astrolabe/stargaze.js
+++ src/astrolabe/stargaze.js
@@ -2,14 +2,17 @@ const { CosmWasmClient } = require("@cosmjs/cosmwasm-stargate");
 const { getConnectionFromToken } = require('./networks')
 
 // Check to see if they pasted a Stargaze Launchpad URL like this:
-// https://app.stargaze.zone/launchpad/stars1m95chyen8tqrawmjhxrewcwscg5vj8pkxzxeaw8em7fnjwth8rusaftwk4
+// https://www.stargaze.zone/launchpad/stars1rkgf7w2cvy7q9rxsu3nsg7hy3gq79vyhmft8gf689fngnlrk8h5qytghmw
+// TO-DO: Ideally this shouldn't be hardcoded, as we've seen the
+// exact path here change before. It would be nice for this to be
+// provided by a library like cosmos-endpoints instead in the future.
 const isStargazeLaunchpadAddress = stargazeUrl => {
-  const stargazeLaunchpadRegex = /^https:\/\/app.stargaze.zone\/launchpad\/stars\w*/;
+  const stargazeLaunchpadRegex = /^https:\/\/(www|app).stargaze.zone\/launchpad\/stars\w*/;
   return stargazeUrl.match(stargazeLaunchpadRegex);
 }
 
 const getCW721FromStargazeLaunchpadUrl = async stargazeUrl => {
-  const stargazeLaunchpadRegex = /^https:\/\/app.stargaze.zone\/launchpad\/(stars\w*)/;
+  const stargazeLaunchpadRegex = /^https:\/\/(www|app).stargaze.zone\/launchpad\/(stars\w*)/;
   const regexMatches = stargazeLaunchpadRegex.exec(stargazeUrl);
   // [0] is the string itself, [1] is the (\w*) capture group
   const otherNftContract = regexMatches[1];
