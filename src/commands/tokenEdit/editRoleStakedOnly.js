1a8af6d1c3d023e6fa8e6543b26ea822db01173d
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-09-30 13:23:51 -0500

    Refactor token-rule edit to not use user input
    This commit drastically simplifies the flow for this command
    from what it was before:
    
       index -> editCheck -> editRoleName -> handleRoleNameEdit
                             editRoleAmount -> handleRoleAmountEdit
                             editRoleStakedOnly -> handleRoleStakedOnly
    into the following:
    
       index -> editRole -> editRoleStakedOnly -> handleRoleEdit
    
    - index now displays a select dropdown instead of requiring the
      user to type in the exact name of the role to edit
    - editRole now opens a modal instead of requiring the user to edit
      the name or amount only, in separate input flows
    - editRoleStakedOnly now validates the data from the modal before
      providing the buttons for staked vs not staked
    - handleRoleEdit now handles updating the token rule with all of
      the requested changes (name, amount, staked or not)

--- src/commands/tokenEdit/editRoleStakedOnly.js
+++ src/commands/tokenEdit/editRoleStakedOnly.js
@@ -1,19 +1,57 @@
 module.exports = {
   editRoleStakedOnly: {
-    getConfig: async () => {
+    getConfig: async (state) => {
+      const { guild, interactionTarget: { fields }, selectedRole, selectedRoleName } = state;
+      // Validate role name selection before continuing
+      const newRoleName = fields.getTextInputValue('role-name');
+      const existingObjectRoles = await guild.roles.fetch();
+      let roleAlreadyExists = existingObjectRoles.some(role => role.name === newRoleName);
+      if (roleAlreadyExists) {
+        // Invalid reply
+        return {
+          error: 'A token role already exists with this name. Please pick a different name, or rename that one first.'
+        }
+      } else {
+        state.newRoleName = newRoleName;
+      }
+
+      // Validate token amount selection before continuing
+      // We only prompt token amount for native and CW20 tokens today
+      if (['native', 'cw20'].includes(selectedRole.token_type)) {
+        const amountOfTokensNeeded = parseInt(fields.getTextInputValue('token-amount'));
+        if (
+          !Number.isInteger(amountOfTokensNeeded) ||
+          amountOfTokensNeeded <= 0
+        ) {
+          // Invalid reply
+          return {
+            error: 'Need a positive number of tokens.'
+          }
+        }
+
+        // We'll multiply by the decimal before we update it
+        state.amountOfTokensNeeded = amountOfTokensNeeded;
+      }
+
       return {
+        next: 'handleRoleEdit',
         prompt: {
           type: 'button',
           title: 'Count only staked tokens?',
+          description: selectedRole.count_staked_only ?
+            `${selectedRoleName} currently counts Staked Tokens only` :
+            `${selectedRoleName} currently does NOT count Staked Tokens only`,
           options: [{
-            next: 'handleRoleStakedOnlyEditYes',
-            label: 'Yes'
+            label: 'Yes',
+            value: 'yes',
           }, {
-            next: 'handleRoleStakedOnlyEditNo',
-            label: 'No, count them all'
+            label: 'No, count them all',
+            value: 'no',
           }],
           footer: {
-            text: 'If you select "No" it will count liquid, staked, and currently unbonding where applicable.',
+            text: selectedRole.count_staked_only ?
+            `If you select "No" it will count liquid, staked, and currently unbonding where applicable.\nIf you select "Yes", there will be no change.` :
+            `If you select "Yes", it will count staked tokens only.\nIf you select "No", there will be no change.`,
           }
         }
       }
