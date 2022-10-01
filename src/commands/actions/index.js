f758fd234e1d508248a99e9b84940b3c2473ee07
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-10-01 15:04:50 -0500

    Port exportCsv, tokenList into action

--- src/commands/actions/index.js
+++ src/commands/actions/index.js
@@ -1,7 +1,11 @@
 const { join } = require("./join");
 const { healthCheck } = require("./healthCheck");
+const { exportCsv } = require("./exportCsv");
+const { tokenList } = require("./tokenList");
 
 module.exports = {
+  exportCsv,
   join,
   healthCheck,
+  tokenList,
 }
--- src/commands/tokenList/index.js
+++ src/commands/tokenList/index.js
@@ -3,21 +3,6 @@ module.exports = {
     ephemeral: true,
     name: 'list',
     description: 'List all token rules for this guild',
-    getConfig: async ({ guildId }, { db: { rolesGet } }) => {
-      let roles = await rolesGet(guildId);
-      const title = `${roles.length} roles found`;
-      const description = roles.length > 0 ?
-        `${roles.map(role => {
-            const roleName = role.give_role;
-            const roleAmt = role.has_minimum_of;
-            const roleDecimals = role.decimals;
-            return `★ ${roleName}\n - min: ${(roleAmt / (10 ** roleDecimals)) }\n- count staked only: ${role.count_staked_only ?? false}\n`;
-          }).join('')}` :
-        `This will be way more exciting when roles are added :)`;
-      return {
-        ephemeral: true,
-        done: { title, description }
-      };
-    }
+    action: 'tokenList',
   }
 }
