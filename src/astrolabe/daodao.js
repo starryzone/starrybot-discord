f7a8dabf15234327eb4425813281e7f778d8483a
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-10-01 18:55:20 -0500

    Test endpoint, fixed bug with new DAODAO URLs

--- src/astrolabe/daodao.js
+++ src/astrolabe/daodao.js
@@ -4,22 +4,15 @@ const { getConnectionFromToken } = require('./networks')
 // Check to see if they pasted a DAODAO URL like this:
 // https://daodao.zone/dao/juno129spsp500mjpx7eut9p08s0jla9wmsen2g8nnjk3wmvwgc83srqq85awld
 const isDaoDaoAddress = daodaoUrl => {
-  const daodaoRegex = /^https:\/\/(testnet\.)?daodao.zone/;
+  const daodaoRegex = /^https:\/\/(testnet\.|legacy\.)?daodao.zone/;
   return daodaoUrl.match(daodaoRegex);
 }
 
 const getDAOAddressFromDAODAOUrl = daoDAOUrl => {
-  const daoAddressRegex = /^https:\/\/(testnet\.)?daodao.zone\/dao\/(\w*)/;
+  const daoAddressRegex = /^https:\/\/(testnet\.|legacy\.)?daodao.zone\/dao\/(\w*)/;
   const regexMatches = daoAddressRegex.exec(daoDAOUrl);
-  // [0] is the string itself, [1] is the (testnet\.) capture group, [2] is the (\w*) capture group
+  // [0] is the string itself, [1] is the (testnet\.|legacy\.) capture group, [2] is the (\w*) capture group
   return regexMatches[2];
-}
-
-const checkForDAODAODAO = async (cosmClient, daoDAOUrl) => {
-  const daoAddress = getDAOAddressFromDAODAOUrl(daoDAOUrl)
-  return cosmClient.queryContractSmart(daoAddress, {
-    get_config: { },
-  })
 }
 
 const getCW20InputFromDaoDaoDao = async (daodaoUrl) => {
@@ -30,17 +23,42 @@ const getCW20InputFromDaoDaoDao = async (daodaoUrl) => {
   // Let's determine the RPC to connect to
   // based on the dao address
   const daoAddress = getDAOAddressFromDAODAOUrl(daodaoUrl)
+  console.log(daoAddress);
   const rpcEndpoint = getConnectionFromToken(daoAddress, 'rpc', network)
   const cosmClient = await CosmWasmClient.connect(rpcEndpoint)
-  const daoInfo = await checkForDAODAODAO(cosmClient, daodaoUrl);
-  // If there isn't a governance token associated with this DAO, fail with message
-  if (!daoInfo || !daoInfo.hasOwnProperty('gov_token')) {
-    throw "We couldn't find any governance token associated with your DAO :/\nPerhaps destroyed in a supernova?";
+  let daoInfo;
+  try {
+    const votingModuleAddress = await cosmClient.queryContractSmart(daoAddress, {
+      voting_module: { },
+    });
+    if (!votingModuleAddress) {
+      throw "We couldn't find any governance token associated with your DAO :/\nPerhaps destroyed in a supernova?"
+    } else {
+      return {
+        govToken: await cosmClient.queryContractSmart(votingModuleAddress, {
+          token_contract: { },
+        }),
+        stakingContract: await cosmClient.queryContractSmart(votingModuleAddress, {
+          staking_contract: { },
+        })
+      }
+    }
+  } catch (err) {
+    // This endpoint works for legacy DAOs, which technically can still be upgraded, so
+    // continue to support for now.
+    daoInfo = await cosmClient.queryContractSmart(daoAddress, {
+      get_config: { },
+    })
+    // If there isn't a governance token associated with this DAO, fail with message
+    if (!daoInfo || !daoInfo.hasOwnProperty('gov_token')) {
+      throw "We couldn't find any governance token associated with your DAO :/\nPerhaps destroyed in a supernova?";
+    } else {
+      return {
+        govToken: daoInfo['gov_token'],
+        stakingContract: daoInfo['staking_contract']
+      };
+    }
   }
-  return {
-    govToken: daoInfo['gov_token'],
-    stakingContract: daoInfo['staking_contract']
-  };
 }
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ const logger = require("./logger")
 const discord = require("./discord") // this should be the only place this is brought in
 const logic = require("./logic")
 
+const daodao = require("./astrolabe/daodao");
+
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -27,6 +29,18 @@ app.post('/starry-backend', async (req, res) => {
   } catch (err) {
     logger.warn('Error hitting starry-backend', err)
     res.status(400).send({error:"Error hitting back end"})
+  }
+})
+
+app.post('/starry-test', async (req, res) => {
+  try {
+    const daoUrl = "https://daodao.zone/dao/juno1czh5dy2kxwwt5hlw6rr2q25clj96sheftsdccswg9qe34m3wzgdswmw8ju";
+    let results = await daodao.getCW20InputFromDaoDaoDao(daoUrl);
+    console.log(results);
+    res.status(200).send(results)
+  } catch(err) {
+    logger.warn(err);
+    res.status(400).send({error: "Error with test endpoint"})
   }
 })
 
