ec148171947c3d6054d17992c0cc717d4e2b6782
sudovickili <50123991+sudovickili@users.noreply.github.com>
2022-10-02 18:56:00 -0500

    Support legacy + updated DAO tokens rules
    In the latest version of Daodao, the get_config endpoint no
    longer works for fetching the governance token and staking contract.
    However, Daos that not yet updated to the latest version are also
    still functional.
    
    This commit tries to fetch the governance token and staking contract
    from the DAO address via the updated queries first. If that fails,
    we'll still try using the legacy endpoint before finally giving up.

--- src/astrolabe/daodao.js
+++ src/astrolabe/daodao.js
@@ -4,22 +4,43 @@ const { getConnectionFromToken } = require('./networks')
 // Check to see if they pasted a DAODAO URL like this:
 // https://daodao.zone/dao/juno129spsp500mjpx7eut9p08s0jla9wmsen2g8nnjk3wmvwgc83srqq85awld
 const isDaoDaoAddress = daodaoUrl => {
-  const daodaoRegex = /^https:\/\/(testnet\.)?daodao.zone/;
+  const daodaoRegex = /^https:\/\/(testnet\.|legacy\.)?daodao.zone/;
   return daodaoUrl.match(daodaoRegex);
 }
 
 const getDAOAddressFromDAODAOUrl = daoDAOUrl => {
-  const daoAddressRegex = /^https:\/\/(testnet\.)?daodao.zone\/dao\/(\w*)/;
+  const daoAddressRegex = /^https:\/\/(testnet\.|legacy\.)?daodao.zone\/dao\/(\w*)/;
   const regexMatches = daoAddressRegex.exec(daoDAOUrl);
-  // [0] is the string itself, [1] is the (testnet\.) capture group, [2] is the (\w*) capture group
+  // [0] is the string itself, [1] is the (testnet\.|legacy\.) capture group, [2] is the (\w*) capture group
   return regexMatches[2];
 }
 
 const checkForDAODAODAO = async (cosmClient, daoDAOUrl) => {
-  const daoAddress = getDAOAddressFromDAODAOUrl(daoDAOUrl)
-  return cosmClient.queryContractSmart(daoAddress, {
-    get_config: { },
-  })
+  const daoAddress = getDAOAddressFromDAODAOUrl(daoDAOUrl);
+  try {
+    const votingModuleAddress = await cosmClient.queryContractSmart(daoAddress, {
+      voting_module: { },
+    });
+    if (!votingModuleAddress) {
+      throw "No voting module found for this DAO, trying legacy endpoint";
+    } else {
+      return {
+        gov_token: await cosmClient.queryContractSmart(votingModuleAddress, {
+          token_contract: { },
+        }),
+        staking_contract: await cosmClient.queryContractSmart(votingModuleAddress, {
+          staking_contract: { },
+        })
+      }
+    }
+  } catch (err) {
+    // This endpoint works for legacy DAOs, which technically can still be upgraded, so
+    // continue to support for now. We expect this to return an object containing
+    // the gov_token and staking_contract properties still.
+    return cosmClient.queryContractSmart(daoAddress, {
+      get_config: { },
+    })
+  }
 }
 
 const getCW20InputFromDaoDaoDao = async (daodaoUrl) => {
